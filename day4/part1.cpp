7a538aa13b405e6f69fac8dfa54ad9ea9f5405c2
schukark <schukark2004@gmail.com>
2023-12-12 15:59:58 +0300

    day 4 complete

---
+++ day4/part1.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <cassert>
+
+std::ifstream fin("input.txt");
+
+int64_t get_result(const std::string& game) {
+    std::stringstream iss(game);
+
+    std::string trash;
+    char c;
+
+    iss >> trash;
+    assert(trash == "Card");
+
+    while (c != ':') iss >> c;
+
+    std::unordered_set<int> winning_words;
+
+    while (iss.peek() != '|') {
+        int number;
+        iss >> number;
+        iss.get();
+
+        winning_words.insert(number);
+    }
+    iss.get();
+
+    int64_t score = 0;
+
+    while (!iss.eof()) {
+        int number;
+        iss >> number;
+        
+        if (winning_words.count(number) > 0) {
+            if (score == 0) {
+                score = 1;
+            }
+            else {
+                score *= 2;
+            }
+        }
+    }
+
+    return score;
+}
+
+int main() {
+    std::string line;
+
+    int64_t result = 0;
+
+    while (!fin.eof()) {
+        std::getline(fin, line);
+
+        result += get_result(line);
+    }
+
+    std::cout << result << std::endl;
+}
---
+++ day4/part2.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <cassert>
+
+std::ifstream fin("input.txt");
+
+int64_t get_result(const std::string& game) {
+    std::stringstream iss(game);
+
+    std::string trash;
+    char c;
+
+    iss >> trash;
+    assert(trash == "Card");
+
+    while (c != ':') iss >> c;
+
+    std::unordered_set<int> winning_words;
+
+    while (iss.peek() != '|') {
+        int number;
+        iss >> number;
+        iss.get();
+
+        winning_words.insert(number);
+    }
+    iss.get();
+
+    int64_t score = 0;
+
+    while (!iss.eof()) {
+        int number;
+        iss >> number;
+        
+        if (winning_words.count(number) > 0) {
+            score++;
+        }
+    }
+
+    return score;
+}
+
+int main() {
+    std::string line;
+
+    std::vector<int64_t> copies(206, 1);
+    int index = 0;
+
+    while (!fin.eof()) {
+        std::getline(fin, line);
+
+        int64_t tmp = get_result(line);
+
+        for (int i = index + 1; i <= index + tmp; i++) {
+            copies[i] += copies[index];
+        }
+        index++;
+    }
+
+    std::cout << std::accumulate(copies.begin(), copies.begin() + index, 0LL) << std::endl;
+}
