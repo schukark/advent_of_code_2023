e582b7981fe9aa6adf685ad115261992f4598b00
Arckadiy Shchukin <schukark2004@gmail.com>
2023-12-29 21:23:03 +0300

    24.1 solved

--- day23/src/task1.rs
+++ day23/src/task1.rs
@@ -1,19 +1,87 @@
 use std::fs;
 
 pub fn execute() {
-    let binding = fs::read_to_string("txts/input.txt").expect("Can't open file");
+    let binding = fs::read_to_string("txts/sample.txt").expect("Can't open file");
     let contents: Vec<&str> = binding.lines().collect();
     let grid: Vec<Vec<char>> = contents.iter().map(|x| x.chars().collect::<Vec<char>>()).collect();
 
     let (start_x, start_y) = find_start(&grid);
+    let (end_x, end_y) = find_end(&grid);
+
+    let mut visited: Vec<Vec<bool>> = Vec::new();
+
+    for i in 0..grid.len() {
+        visited.push(Vec::new());
+        for _ in 0..grid[0].len() {
+            visited[i].push(false);
+        }
+    }
+
+    let longest_path = dfs(&grid, &mut visited,  0, (start_x, start_y), (end_x, end_y));
+    println!("{longest_path}");
+}
+
+fn dfs(grid: &Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>, 
+    depth: i32, cur_position: (i32, i32), end_position: (i32, i32)) -> i32 {
+
+    visited[cur_position.0 as usize][cur_position.1 as usize] = true;
+
+    if end_position == cur_position {
+        println!("Changed max to {depth}");
+        visited[cur_position.0 as usize][cur_position.1 as usize] = false;
+        return depth;
+    }
+
+    let mut mut_ans = 0;
+
+    for (dx, dy) in vec![(-1, 0), (0, 1), (1, 0), (0, -1)] {
+        let (new_x, new_y) = (cur_position.0 + dx, cur_position.1 + dy);
+
+        if new_x < 0 || new_x >= grid.len() as i32 || new_y < 0 || new_y >= grid[0].len() as i32 {
+            continue;
+        }
+
+        if visited[new_x as usize][new_y as usize] || grid[new_x as usize][new_y as usize] == '#' {
+            continue;
+        }
+
+        if grid[new_x as usize][new_y as usize] == '.' {
+            mut_ans = i32::max(mut_ans, dfs(grid, visited, depth + 1, (new_x, new_y), end_position));
+        }
+        else if get_arrow((dx, dy)) == grid[new_x as usize][new_y as usize] {
+            let (new_x, new_y) = (cur_position.0 + 2 * dx, cur_position.1 + 2 * dy);
+            mut_ans = i32::max(dfs(grid, visited,  depth + 1, (new_x, new_y), end_position), mut_ans);
+        }
+    }
+
+    visited[cur_position.0 as usize][cur_position.1 as usize] = false;
+    return mut_ans;
+}
+
+fn get_arrow(d: (i32, i32)) -> char {
+    match d {
+        (1, 0) => 'v',
+        (-1, 0) => '^',
+        (0, 1) => '>',
+        (0, -1) => '<',
+        _ => panic!("Wrong symbol"),
+    }
 }
 
 fn find_start(grid: &Vec<Vec<char>>) -> (i32, i32) {
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
-            if grid[i][j] == 'S' {
-                return (i as i32, j as i32);
-            }
+    for i in 0..grid[0].len() {
+        if grid[0][i] == '.' {
+            return (0, i as i32);
+        }
+    }
+
+    (-1, -1)
+}
+
+fn find_end(grid: &Vec<Vec<char>>) -> (i32, i32) {
+    for i in 0..grid[grid.len() - 1].len() {
+        if grid[grid.len() - 1][i] == '.' {
+            return (grid.len() as i32 - 1, i as i32);
         }
     }
 
--- day24/src/task1.rs
+++ day24/src/task1.rs
@@ -1,5 +1,109 @@
 use std::fs;
+use std::ops::{Sub};
 
 pub fn execute() {
+    let binding = fs::read_to_string("txts/input.txt").expect("Can't open file");
+    let contents = binding.lines().collect::<Vec<&str>>();
 
+    let mut points: Vec<Entry> = Vec::new();
+
+    for line in contents {
+        points.push(Entry::new(line));
+    }
+
+    println!("{}", points.len());
+
+    let mut count = 0;
+
+    for i in 0..points.len() {
+        for j in i+1..points.len() {
+            if let Some(_) = points[i].intersection2d(&points[j]) {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{count}")
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Point(f64, f64, f64);
+
+impl Point {
+    fn is_parallel2d(&self, other: &Point) -> bool {
+        self.0 * other.1 == self.1 * other.0
+    }
+}
+
+impl Sub for Point {
+    type Output = Point;
+    fn sub(self, other: Point) -> Self::Output {
+        Point(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
+#[derive(Debug)]
+struct Entry {
+    position: Point,
+    velocity: Point
+}
+
+impl Entry {
+    fn new(line: &str) -> Entry {
+        let splitted = line.split(" @ ").map(|x| x.split(",").collect::<Vec<_>>()).collect::<Vec<_>>();
+        let splitted: Vec<Vec<f64>> = splitted.iter().map(|x| x.iter().map(|y| y.trim().parse().unwrap()).collect::<Vec<_>>()).collect::<Vec<_>>();
+        Entry {position: Point(splitted[0][0], splitted[0][1], splitted[0][2]), velocity: Point(splitted[1][0], splitted[1][1], splitted[1][2])}
+    }
+
+    fn intersection2d(&self, other: &Entry) -> Option<Point> {
+        const BOUNDARY: (f64, f64) = (200000000000000.0, 400000000000000.0);
+        /*
+        (x-x_0)/s_x = (y-y_0)/s_y => x*s_y-y*s_x + y_0*s_x-x_0*s_y = 0
+        
+        */
+
+        // First check if they are the same line or parallel
+        if self.velocity.is_parallel2d(&other.velocity) {
+            return None;
+        }
+
+        let (a0, b0, c0) = (self.velocity.1, -self.velocity.0, self.position.1 * self.velocity.0 - self.position.0 * self.velocity.1);
+        let (a1, b1, c1) = (other.velocity.1, -other.velocity.0, other.position.1 * other.velocity.0 - other.position.0 * other.velocity.1);
+
+        //println!("{a0} {b0} {c0}");
+        //println!("{a1} {b1} {c1}");
+
+        // now all the lines are in the form ax+by+c=0
+        // a0x+b0y+c0=0 | * a1
+        // a1x+b1y+c1=0 | * -a0
+
+        // (a1 * b0 - a0 * b1) * y + (c0 * a1 - c1 * a0) = 0
+        let intersection_y = (c1 * a0 - c0 * a1) as f64 / (a1 * b0 - a0 * b1) as f64;
+        //println!("{intersection_y}");
+        if intersection_y < BOUNDARY.0 || intersection_y > BOUNDARY.1 {
+            return None;
+        }
+
+        let intersection_x = (-c0 as f64 - b0 as f64 * intersection_y) / a0 as f64;
+        //println!("{intersection_x}");
+        if intersection_x < BOUNDARY.0 || intersection_x > BOUNDARY.1 {
+            return None;
+        }
+        
+        let result = Point(intersection_x, intersection_y, 0.0);
+
+        let time1 = result - self.position;
+        let time1 = time1.0 / self.velocity.0;
+
+        let time2 = result - other.position;
+        let time2 = time2.0 / other.velocity.0;
+
+        //println!("{time1} {time2}");
+
+        if time1 < 0.0 || time2 < 0.0 {
+            return None;
+        }
+
+        Some(result)
+    }
 }
